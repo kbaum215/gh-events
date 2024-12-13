9fbc98643f850083de43ac923bb229fd7d84e786
kbaumyahoo <kwsbaumer@gmail.com>
2024-12-12 17:48:10 -0800

    update test

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -8,7 +8,7 @@ function MainContent() {
   function toggleHelp() {
     setHelpVisible((isVisible) => !isVisible);
   }
-  // comments
+  // comments blah
   return (
     <main>
       <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
